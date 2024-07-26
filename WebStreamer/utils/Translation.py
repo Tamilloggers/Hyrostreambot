60922de6b7c2370f8e88bbb6b49bea34677905c5
Tamilloggers <tamilloggers@gmail.com>
2024-07-26 19:13:44 +0530

    Update Translation.py

--- WebStreamer/utils/Translation.py
+++ WebStreamer/utils/Translation.py
@@ -36,10 +36,8 @@ class Language:
 
         STREAM_MSG_TEXT: str ="""
 <i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n
-<b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <i>{}</i>\n
-<b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <i>{}</i>\n
-<b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> <i>{}</i>\n
-<b>🖥WATCH :</b> <i>{}</i>\n
+<b>Fɪʟᴇ ɴᴀᴍᴇ :</b> <i>{}</i>\n
+<b>Fɪʟᴇ ꜱɪᴢᴇ :</b> <i>{}</i>\n
 <b>Link Generated Using<b> <a href='https://t.me/{}'>{}</a>"""
 
         BAN_TEXT: str = "__Sᴏʀʀʏ Sɪʀ, Yᴏᴜ ᴀʀᴇ Bᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴍᴇ.__\n\n**[Cᴏɴᴛᴀᴄᴛ Dᴇᴠᴇʟᴏᴘᴇʀ](tg://user?id={}) Tʜᴇʏ Wɪʟʟ Hᴇʟᴘ Yᴏᴜ**"
@@ -82,10 +80,8 @@ Links Left: <code>{}<code>"""
 
         STREAM_MSG_TEXT: str ="""
 <i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n
-<b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <i>{}</i>\n
-<b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <i>{}</i>\n
-<b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> <i>{}</i>\n
-<b>🖥WATCH :</b> <i>{}</i>\n
+<b>Fɪʟᴇ ɴᴀᴍᴇ :</b> <i>{}</i>\n
+<b>Fɪʟᴇ ꜱɪᴢᴇ :</b> <i>{}</i>\n
 <b>Link Generated Using<b> <a href='https://t.me/{}'>{}</a>"""
 
         
