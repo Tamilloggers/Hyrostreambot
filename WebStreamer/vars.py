37359f88497c5c6f2f5a09c0e24077b8c703e328
Tamilloggers <tamilloggers@gmail.com>
2024-08-04 13:21:52 +0530

    Update vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -1,47 +1,30 @@
-# This file is a part of FileStreamBot
-from urllib import request
-from os import environ
-from dotenv import load_dotenv
+from WebStreamer import config_dict
 
-load_dotenv()
-
-
-class Var(object):
-    MULTI_CLIENT = False
-    API_ID = int(environ.get("API_ID","24826284"))
-    API_HASH = str(environ.get("API_HASH","3fe1000ee94fb54fcd30c218c9057434"))
-    BOT_TOKEN = str(environ.get("BOT_TOKEN","6852101667:AAEZkHqs2r6Wqbl8WAGtOR2Jx4rhEZmiLjk"))
-    SLEEP_THRESHOLD = int(environ.get("SLEEP_THRESHOLD", "60"))  # 1 minte
-    WORKERS = int(environ.get("WORKERS", "6"))  # 6 workers = 6 commands at once
-    BIN_CHANNEL = int(
-        environ.get("BIN_CHANNEL", "-1002233287339")
-    )  # you NEED to use a CHANNEL when you're using MULTI_CLIENT
-    PORT = int(environ.get("PORT", 8080))
-    BIND_ADDRESS = str(environ.get("WEB_SERVER_BIND_ADDRESS", "0.0.0.0"))
-    PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    HAS_SSL = str(environ.get("HAS_SSL", "0").lower()) in ("1", "true", "t", "yes", "y")
-    NO_PORT = str(environ.get("NO_PORT", "0").lower()) in ("1", "true", "t", "yes", "y")
-    FQDN = str(environ.get("FQDN", BIND_ADDRESS))
-    URL = "http{}://{}{}/".format(
-            "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT)
-        )
-
-    DATABASE_URL = str(environ.get('DATABASE_URL',"mongodb+srv://fino:fino@cluster0.ko0stef.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"))
-    UPDATES_CHANNEL = str(environ.get('UPDATES_CHANNEL', "Telegram"))
-    OWNER_ID = int(environ.get('OWNER_ID', '7136104373'))
-    SESSION_NAME = str(environ.get('SESSION_NAME', 'hydroBot'))
-    FORCE_UPDATES_CHANNEL = environ.get('FORCE_UPDATES_CHANNEL', False)
-    FORCE_UPDATES_CHANNEL = True if str(FORCE_UPDATES_CHANNEL).lower() == "true" else False
-    ALLOWED_USERS = [x.strip("@ ") for x in str(environ.get("ALLOWED_USERS") or "").split(",") if x.strip("@ ")]
-
-    KEEP_ALIVE = str(environ.get("KEEP_ALIVE", "0").lower()) in  ("1", "true", "t", "yes", "y")
-    IMAGE_FILEID = environ.get('IMAGE_FILEID', "https://deekshith.eu.org/static/MyFiles.png")
-    TOS = environ.get("TOS", None)
-    if TOS:
-        response = request.urlopen(TOS)
-        data = response.read().decode('utf-8')
-        TOS = data.strip()
-
-    MODE = environ.get("MODE", "primary")
-    SECONDARY = True if MODE.lower() == "secondary" else False
-    LINK_LIMIT = int(environ.get("LINK_LIMIT")) if "LINK_LIMIT" in environ else None
+'MULTI_CLIENT': config_dict['MULTI_CLIENT'],
+'API_ID': config_dict['API_ID'],
+'API_HASH': config_dict['API_HASH'],
+'BOT_TOKEN': config_dict['BOT_TOKEN'],
+'SLEEP_THRESHOLD': config_dict['SLEEP_THRESHOLD'],
+'WORKERS': config_dict['WORKERS'],
+'BIN_CHANNEL': config_dict['BIN_CHANNEL'],
+'PORT': config_dict['PORT'],
+'BIND_ADDRESS': config_dict['BIND_ADDRESS'],
+'PING_INTERVAL': config_dict['PING_INTERVAL'],
+'HAS_SSL': config_dict['HAS_SSL'],
+'NO_PORT': config_dict['NO_PORT'],
+'FQDN': config_dict['FQDN'],
+'URL': config_dict['URL'],
+'DATABASE_URL': config_dict['DATABASE_URL'],
+'UPDATES_CHANNEL': config_dict['UPDATES_CHANNEL'],
+'OWNER_ID': config_dict['OWNER_ID'],
+'SESSION_NAME': config_dict['SESSION_NAME'],
+'FORCE_UPDATES_CHANNEL': config_dict['FORCE_UPDATES_CHANNEL'],
+'ALLOWED_USERS': config_dict['ALLOWED_USERS'],
+'KEEP_ALIVE': config_dict['KEEP_ALIVE'],
+'IMAGE_FILEID': config_dict['IMAGE_FILEID'],
+'TOS': config_dict['TOS'],
+'MODE': config_dict['MODE'],
+'SECONDARY': config_dict['SECONDARY'],
+'LINK_LIMIT': config_dict['LINK_LIMIT'],
+'UPSTREAM_REPO': config_dict['UPSTREAM_REPO'],
+'UPSTREAM_BRANCH': config_dict['UPSTREAM_BRANCH'],
