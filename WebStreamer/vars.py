6772a123705665d98418d3f3540c2ede5cb5ee1d
Tamilloggers <tamilloggers@gmail.com>
2024-08-04 13:25:09 +0530

    Update vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -1,30 +1,31 @@
 from WebStreamer import config_dict
 
-'MULTI_CLIENT': config_dict['MULTI_CLIENT'],
-'API_ID': config_dict['API_ID'],
-'API_HASH': config_dict['API_HASH'],
-'BOT_TOKEN': config_dict['BOT_TOKEN'],
-'SLEEP_THRESHOLD': config_dict['SLEEP_THRESHOLD'],
-'WORKERS': config_dict['WORKERS'],
-'BIN_CHANNEL': config_dict['BIN_CHANNEL'],
-'PORT': config_dict['PORT'],
-'BIND_ADDRESS': config_dict['BIND_ADDRESS'],
-'PING_INTERVAL': config_dict['PING_INTERVAL'],
-'HAS_SSL': config_dict['HAS_SSL'],
-'NO_PORT': config_dict['NO_PORT'],
-'FQDN': config_dict['FQDN'],
-'URL': config_dict['URL'],
-'DATABASE_URL': config_dict['DATABASE_URL'],
-'UPDATES_CHANNEL': config_dict['UPDATES_CHANNEL'],
-'OWNER_ID': config_dict['OWNER_ID'],
-'SESSION_NAME': config_dict['SESSION_NAME'],
-'FORCE_UPDATES_CHANNEL': config_dict['FORCE_UPDATES_CHANNEL'],
-'ALLOWED_USERS': config_dict['ALLOWED_USERS'],
-'KEEP_ALIVE': config_dict['KEEP_ALIVE'],
-'IMAGE_FILEID': config_dict['IMAGE_FILEID'],
-'TOS': config_dict['TOS'],
-'MODE': config_dict['MODE'],
-'SECONDARY': config_dict['SECONDARY'],
-'LINK_LIMIT': config_dict['LINK_LIMIT'],
-'UPSTREAM_REPO': config_dict['UPSTREAM_REPO'],
-'UPSTREAM_BRANCH': config_dict['UPSTREAM_BRANCH'],
+MULTI_CLIENT=config_dict['MULTI_CLIENT']
+API_ID=config_dict['API_ID']
+API_HASH=config_dict['API_HASH']
+BOT_TOKEN=config_dict['BOT_TOKEN']
+SLEEP_THRESHOLD=config_dict['SLEEP_THRESHOLD']
+WORKERS=config_dict['WORKERS']
+BIN_CHANNEL=config_dict['BIN_CHANNEL']
+PORT=config_dict['PORT']
+BIND_ADDRESS=config_dict['BIND_ADDRESS']
+PING_INTERVAL=config_dict['PING_INTERVAL']
+HAS_SSL=config_dict['HAS_SSL']
+NO_PORT=config_dict['NO_PORT']
+FQDN=config_dict['FQDN']
+URL=config_dict['URL']
+DATABASE_URL=config_dict['DATABASE_URL']
+UPDATES_CHANNEL=config_dict['UPDATES_CHANNEL']
+OWNER_ID=config_dict['OWNER_ID']
+SESSION_NAME=config_dict['SESSION_NAME']
+FORCE_UPDATES_CHANNEL=config_dict['FORCE_UPDATES_CHANNEL']
+ALLOWED_USERS=config_dict['ALLOWED_USERS']
+KEEP_ALIVE=config_dict['KEEP_ALIVE']
+IMAGE_FILEID=config_dict['IMAGE_FILEID']
+TOS=config_dict['TOS']
+MODE=config_dict['MODE']
+SECONDARY=config_dict['SECONDARY']
+LINK_LIMIT=config_dict['LINK_LIMIT']
+UPSTREAM_REPO=config_dict['UPSTREAM_REPO']
+UPSTREAM_BRANCH=config_dict['UPSTREAM_BRANCH']
+
